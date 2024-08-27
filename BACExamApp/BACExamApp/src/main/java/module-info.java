8536ccdee9f80781b61115e8843aa33aa04f9a46
MoctarTraore <164496395+MoctarTraore@users.noreply.github.com>
2024-08-27 10:09:32 -0500

    Delete BACExamApp directory

--- BACExamApp/BACExamApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.btsexamapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.btsexamapp to javafx.fxml;
-    exports com.example.btsexamapp;
-}
